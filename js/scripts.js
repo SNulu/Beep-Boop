6700e03e4962370a1c16570a365e0cdc15e71529
Snulu <Nnabwami@gmail.com>
2019-01-25 09:42:03 -0800

    declare variables in js scripts

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,33 @@
 
 
 
+$(document).ready(function() {
+  $("#formOne").submit(function(event){
+
+    var number1 = parenInt($("number1")).val());
+    var number2 = parenInt($("number2")).val());
+    var number1 = parenInt($("number1")).val());
+
+
+
+
+    event.preventDefault();
+    console.log(listsArr);
+  });
+});
+
+
+
 //
+  //  var listsArr = [];
+    //for (var i = 1; i <= 5; i++){
+  //    listsArr.push($("#item"+i).val());
+  //  }
+  //  listsArr = listsArr.map(function(grow){
+    //return grow.toUpperCase();
+    //})
+    //listsArr = listsArr.sort();
+    //for (var i = 0; i < listsArr.length; i++) {
+    //  $("ul").append("<li>" +listsArr[i] +"</li>");
+    //}
+    //$(".form-group").toggle();
