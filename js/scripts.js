040873336990825cceebd44824f081898c4e5c39
Snulu <Nnabwami@gmail.com>
2019-01-25 10:11:58 -0800

    add/declare if statements for number 3 and number 2.

--- js/scripts.js
+++ js/scripts.js
@@ -1,14 +1,28 @@
+function beepIn(input) {
+    var number = [];
+    if(input >= 3)
+{
+  while (input >= 3)
+  {
+    numbers.push("I'm Sorry, Dave. I'm afraid I can't do that")
+  }
+}
 
-
+if (input >= 2) {
+  ()
+}
 
 $(document).ready(function() {
   $("#formOne").submit(function(event){
 
-    var number1 = parenInt($("number1")).val());
-    var number2 = parenInt($("number2")).val());
-    var number1 = parenInt($("number3")).val());
+    var input = $("#number1").val();
+    var input = BeepIn(parseInt(input));
 
-    fo
+    //var number1 = parenInt($("number1")).val());
+    //var number2 = parenInt($("number2")).val());
+    //var number1 = parenInt($("number3")).val());
+
+    //for (var )
 
 
 
